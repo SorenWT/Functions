e810432cfecef5a2dd15c5b7a6693306fcd6cda9
SorenWT <sorenwt@gmail.com>
2020-03-13 20:24:02 -0400

    stim_TE shift stats added

--- stim_TE.m
+++ stim_TE.m
@@ -1,4 +1,4 @@
-function teresults = stim_TE(braints,musicts,fs,labels,lags,stats)
+function teresults = stim_TE(braints,musicts,fs,labels,lags,shiftstats)
 % Inputs: 
 %     braints: a 2D or 3D matrix of the measure time series in the form
 %        channels x time points x music pieces (can do only one music piece
@@ -13,16 +13,16 @@ function teresults = stim_TE(braints,musicts,fs,labels,lags,stats)
 %        will be extracted from the latter)
 %     lags: a scalar or vector of lags (in samples) at which to compute
 %        transfer entropy (default = 1:ceil(fs))
-%     stats: 'yes' or 'no', do statistics on the data (only put 'yes' if
-%        you are inputting a 3D matrix with multiple music pieces; default
-%        = 'no')
+%     shiftstats: 'yes' or 'no', normalize single-trial values by
+%        time-shifted surrogates (default = 'yes' if only one trial input -
+%        otherwise trial-shuffling is implemented instead)
 %
 % Outputs: 
 %     teresults: transfer entropy results. Important fields are:
 %         TEmat: the actual transfer entropy values. The format is channels
-%         x music pieces
+%         x music pieces, or channels x shifts for single-trial data
 %         MImat: mutual information values. Format is the same as TEmat
-%         (channels x music pieces)
+%         (channels x music pieces/shifts)
 
 
 if size(braints,1) > size(braints,2)
@@ -38,7 +38,11 @@ if ~exist('lags','var')
 end
 
 if ~exist('stats','var')
-   stats = 'no'; 
+    if size(braints,3) == 1
+        shiftstats = 'yes';
+    else
+        shiftstats = 'no';
+    end
 end
 
 labels = vert(labels);
@@ -49,6 +53,15 @@ for i = 1:size(braints, 3)
    data.trial{i} = [braints(:,:,i); musicts(i,:)]; 
    data.time{i} = linspace(0,length(braints)/fs,length(braints));
 end
+
+if size(braints,3) == 1
+   shifttimes = [(max(lags)+ceil(0.25*max(lags))):(max(lags)+5*length(lags))+ceil(0.25*max(lags))-1];
+   for i = 1:length(shiftimes)
+        data.trial{i+1} = [braints; musicts([shifttimes(i):end 1:(shifttimes(i)-1)])];
+        data.time{i} = linspace(0,length(braints)/fs,length(braints));
+   end
+end
+
 data.label = [labels; {'Music'}];
 data.fsample = fs;
 
@@ -66,13 +79,13 @@ for i = 1:length(lags)
     cfg.sgncmb = cat(2,repmat({'Music'},length(labels),1),vert(labels));
     cfg.toi = [-Inf Inf];
     cfg.ragtaurange = [0.2 0.5]; cfg.ragdim = 2:12; cfg.actthrvalue = length(musicts);
-    cfg.minnrtrials = length(data.trial); cfg.maxlag = 0.5*length(musicts); cfg.repPred = ceil(0.2*length(musicts));
+    cfg.minnrtrials = length(data.trial); cfg.maxlag = ceil(0.5*length(musicts)); cfg.repPred = ceil(0.2*length(musicts));
     cfg.predicttime_u = lags(i);
     cfg.flagNei = 'Mass'; cfg.sizeNei = 4;
     
     dataprep = TEprepare(cfg,data);
     
-    if strcmpi(stats,'yes')
+    if strcmpi(shiftstats,'no')
         cfg = []; cfg.optdimusage = 'maxdim';
         cfg.fileidout = 'test';
         cfg.surrogatetype = 'trialshuffling';
@@ -87,6 +100,8 @@ for i = 1:length(lags)
         cfg.calctime = 'no'; cfg.shuffle = 'no';
         
         teresults{i} = transferentropy(cfg,dataprep);
+        teresults{i}.te_z = zscore(teresults{i}.TEmat,[],2);
+        teresults{i}.mi_z = zscore(teresults{i}.MImat,[],2);
     end
 end
 
