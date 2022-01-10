43b55276a41e758c7c13a90257c5f5c431bb3cf3
SorenWT <sorenwt@gmail.com>
2022-01-10 08:32:40 -0500

    Added try/catch to CamCAN_IRASA_tf_cruncher, modified gitignore not to track .m~ files

--- CamCAN_IRASA_tf_cruncher.m
+++ CamCAN_IRASA_tf_cruncher.m
@@ -25,63 +25,92 @@ load('atlas_MMP1.0_4k.mat')
 load('mmp_atlas_grouped.mat')
 
 %parpool(48)
+%count = 1;
+badcount = 1;
+
 for i = indx
+    %i = indx(count);
+    
     subid = char(extractBefore(files(i).name,'_'));
     try
         grad = parload(fullfile('/group/northoff/share/camcan/Task/Continuous/',[subid '_continuous_1Hz.mat']),'cont_data');
         grad = grad.grad;
+    catch
+        badsubs(badcount).message = 'No continuous task file found';
+        badsubs(badcount).indx = i;
+        badcount = badcount+1;
+        continue
+    end
+    
+    
+    if ~exist(['/home/soren/Documents/camcan/source/task/epoched/' files(i).name(1:end-4) '_roidata_mmpreduced.mat'],'file')
+        load(files(i).name)
+        data.grad = grad;
         
         
-        if ~exist(['/home/soren/Documents/camcan/source/task/epoched/' files(i).name(1:end-4) '_roidata_mmpreduced.mat'],'file')
-            load(files(i).name)
-            data.grad = grad;
-            
-            
+        try
             headmodel = parload(fullfile('/home/soren/Documents/camcan/sourcemodels/',subid,'sourcemodel',...
                 [subid '_headmodel.mat']),'vol');
+        catch
+            badsubs(badcount).message = 'No headmodel file found';
+            badsubs(badcount).indx = i;
+            badcount = badcount+1;
+            continue
+        end
+        try
             sourcemodel = parload(fullfile('/home/soren/Documents/camcan/sourcemodels/',subid,'sourcemodel',...
                 [subid '_sourcemodel_8k_nmg.mat']),'sourcemodel');
-            
-            roidata = SourceEst(data,headmodel,sourcemodel,atlas,struct('parflag',1));
-            
-            roidata.sampleinfo = data.sampleinfo; roidata.trialinfo = data.trialinfo;
-            
-            save(['/home/soren/Documents/camcan/source/task/epoched/' files(i).name(1:end-4) '_roidata_mmp.mat'],'roidata')
-            
-            roidata = mmp_reassign(roidata,mmpgroup);
-            roidata.sampleinfo = data.sampleinfo; roidata.trialinfo = data.trialinfo;
-
-            
-            save(['/home/soren/Documents/camcan/source/task/epoched/' files(i).name(1:end-4) '_roidata_mmpreduced.mat'],'roidata')
-            
-            data = roidata;
-        else
-            load(['/home/soren/Documents/camcan/source/task/epoched/' files(i).name(1:end-4) '_roidata_mmpreduced.mat'])
-            data = roidata;
+        catch
+            badsubs(badcount).message = 'No headmodel file found';
+            badsubs(badcount).indx = i;
+            badcount = badcount+1;
+            continue
         end
         
+        roidata = SourceEst(data,headmodel,sourcemodel,atlas,struct('parflag',1));
         
-        if ~exist([files(i).name '_IRASAtf.mat'],'file')
-            %load(files(i).name)
-            
-            latencies = data.sampleinfo(:,1)'+1000;
-            
-            itis = diff([latencies(1) latencies]); % don't keep the first trial since you don't know how long the first ITI really was
-            goodevents = find(itis >=2000); % keep trials where the preceeding iti was 4 seconds or longer
-            %ntrl(i) = length(goodevents);
-            
-            data = ft_selectdata(struct('trials',goodevents),data);
-            
-            tic;
-            [osci,specs] = IRASA_tf(cfg,data);
-            eltime(i) = toc;
-            cfg.oscifrac = 'frac';
-            frac = IRASA_tf(cfg,data,specs);
-            cfg.oscifrac = 'mixd';
-            mixd = IRASA_tf(cfg,data,specs);
-            save([files(i).name '_IRASAtf.mat'],'osci','frac','mixd','specs')
-        end
-    catch
-        badsubs(i) = 1;
+        roidata.sampleinfo = data.sampleinfo; roidata.trialinfo = data.trialinfo;
+        
+        save(['/home/soren/Documents/camcan/source/task/epoched/' files(i).name(1:end-4) '_roidata_mmp.mat'],'roidata')
+        
+        roidata = mmp_reassign(roidata,mmpgroup);
+        roidata.sampleinfo = data.sampleinfo; roidata.trialinfo = data.trialinfo;
+        
+        
+        save(['/home/soren/Documents/camcan/source/task/epoched/' files(i).name(1:end-4) '_roidata_mmpreduced.mat'],'roidata')
+        
+        data = roidata;
+    else
+        load(['/home/soren/Documents/camcan/source/task/epoched/' files(i).name(1:end-4) '_roidata_mmpreduced.mat'])
+        data = roidata;
     end
+    
+    
+    if ~exist([files(i).name '_IRASAtf_source.mat'],'file')
+        %load(files(i).name)
+        
+        latencies = data.sampleinfo(:,1)'+1000;
+        
+        itis = diff([latencies(1) latencies]); % don't keep the first trial since you don't know how long the first ITI really was
+        goodevents = find(itis >=2000); % keep trials where the preceeding iti was 4 seconds or longer
+        %ntrl(i) = length(goodevents);
+        
+        data = ft_selectdata(struct('trials',goodevents),data);
+        
+        disp(['Data has ' num2str(length(data.trial)) ' trials'])
+        
+        tic;
+        [osci,specs] = IRASA_tf(cfg,data);
+        eltime(i) = toc;
+        cfg.oscifrac = 'frac';
+        frac = IRASA_tf(cfg,data,specs);
+        cfg.oscifrac = 'mixd';
+        mixd = IRASA_tf(cfg,data,specs);
+        save(['/home/soren/Documents/camcan/source/task/epoched/' files(i).name '_IRASAtf_source.mat'],'osci','frac','mixd','specs')
+    end
+    
+    count = count+1;
+    %catch
+    %   badsubs(i) = 1;
+    %end
 end
