693e89e5661c51579120817b420296d69296030f
SorenWT <sorenwt@gmail.com>
2024-03-12 01:04:04 -0400

    Replaced ICC function

--- ICC/ICC.m
+++ ICC/ICC.m
@@ -1,88 +1,188 @@
-function out = ICC(cse,typ,dat)
-%function to work out ICCs according to shrout & fleiss' schema (Shrout PE,
-%Fleiss JL. Intraclass correlations: uses in assessing rater reliability.
-%Psychol Bull. 1979;86:420-428).
+function [r, LB, UB, F, df1, df2, p] = ICC(M, type, alpha, r0)
+% Intraclass correlation
+%   [r, LB, UB, F, df1, df2, p] = ICC(M, type, alpha, r0)
 %
-% 'dat' is data whose rows represent different ratings or raters & whose
-% columns represent different cases or targets being measured. Each target
-% is assumed to be a random sample from a population of targets.
+%   M is matrix of observations. Each row is an object of measurement and
+%   each column is a judge or measurement.
 %
-% 'cse' is either 1,2,3. 'cse' is: 1 if each target is measured by a
-% different set of raters from a population of raters, 2 if each target is
-% measured by the same raters, but that these raters are sampled from a
-% population of raters, 3 if each target is measured by the same raters and
-% these raters are the only raters of interest.
+%   'type' is a string that can be one of the six possible codes for the desired
+%   type of ICC:
+%       '1-1': The degree of absolute agreement among measurements made on
+%         randomly seleted objects. It estimates the correlation of any two
+%         measurements.
+%       '1-k': The degree of absolute agreement of measurements that are
+%         averages of k independent measurements on randomly selected
+%         objects.
+%       'C-1': case 2: The degree of consistency among measurements. Also known
+%         as norm-referenced reliability and as Winer's adjustment for
+%         anchor points. case 3: The degree of consistency among measurements maded under
+%         the fixed levels of the column factor. This ICC estimates the
+%         corrlation of any two measurements, but when interaction is
+%         present, it underestimates reliability.
+%       'C-k': case 2: The degree of consistency for measurements that are
+%         averages of k independent measurements on randomly selected
+%         onbjectgs. Known as Cronbach's alpha in psychometrics. case 3:  
+%         The degree of consistency for averages of k independent
+%         measures made under the fixed levels of column factor.
+%       'A-1': case 2: The degree of absolute agreement among measurements. Also
+%         known as criterion-referenced reliability. case 3: The absolute 
+%         agreement of measurements made under the fixed levels of the column factor.
+%       'A-k': case 2: The degree of absolute agreement for measurements that are
+%         averages of k independent measurements on randomly selected objects.
+%         case 3: he degree of absolute agreement for measurements that are
+%         based on k independent measurements maded under the fixed levels
+%         of the column factor.
 %
-% 'typ' is either 'single' or 'k' & denotes whether the ICC is based on a
-% single measurement or on an average of k measurements, where k = the
-% number of ratings/raters.
+%       ICC is the estimated intraclass correlation. LB and UB are upper
+%       and lower bounds of the ICC with alpha level of significance. 
 %
-% This has been tested using the example data in the paper by shrout & fleiss.
-% 
-% Example: out = ICC(3,'k',S_Fdata)
-% returns ICC(3,k) of data 'S_Fdata' to double 'out'.
+%       In addition to estimation of ICC, a hypothesis test is performed
+%       with the null hypothesis that ICC = r0. The F value, degrees of
+%       freedom and the corresponding p-value of the this test are
+%       reported.
 %
-% Kevin Brownhill, Imaging Sciences, KCL, London kevin.brownhill@kcl.ac.uk
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%       (c) Arash Salarian, 2008
+%
+%       Reference: McGraw, K. O., Wong, S. P., "Forming Inferences About
+%       Some Intraclass Correlation Coefficients", Psychological Methods,
+%       Vol. 1, No. 1, pp. 30-46, 1996
+%
 
-dat = rmnan(dat,1);
-
-%number of raters/ratings
-k = size(dat,2);
-%number of targets
-n = size(dat,1);
-%mean per target
-mpt = mean(dat,2);
-%mean per rater/rating
-mpr = mean(dat);
-%get total mean
-tm = mean(mpt);
-%within target sum sqrs
-WSS = sum(sum(bsxfun(@minus,dat,mpt).^2));
-%within target mean sqrs
-WMS = WSS / (n * (k - 1));
-%between rater sum sqrs
-RSS = sum((mpr - tm).^2) * n;
-%between rater mean sqrs
-RMS = RSS / (k - 1);
-% %get total sum sqrs
-% TSS = sum(sum((dat - tm).^2));
-%between target sum sqrs
-BSS = sum((mpt - tm).^2) * k;
-%between targets mean squares
-BMS = BSS / (n - 1);
-%residual sum of squares
-ESS = WSS - RSS;
-%residual mean sqrs
-EMS = ESS / ((k - 1) * (n - 1));
-switch cse
-    case 1
-        switch typ
-            case 'single'
-                out = (BMS - WMS) / (BMS + (k - 1) * WMS);
-            case 'k'
-                out = (BMS - WMS) / BMS;
-            otherwise
-               error('Wrong value for input typ') 
-        end
-    case 2
-        switch typ
-            case 'single'
-                out = (BMS - EMS) / (BMS + (k - 1) * EMS + k * (RMS - EMS) / n);
-            case 'k'
-                out = (BMS - EMS) / (BMS + (RMS - EMS) / n);
-            otherwise
-               error('Wrong value for input typ') 
-        end
-    case 3
-        switch typ
-            case 'single'
-                out = (BMS - EMS) / (BMS + (k - 1) * EMS);
-            case 'k'
-                out = (BMS - EMS) / BMS;
-            otherwise
-               error('Wrong value for input typ') 
-        end
-    otherwise
-        error('Wrong value for input cse')
+if nargin < 3
+    alpha = .05;
 end
+
+if nargin < 4
+    r0 = 0;
+end
+
+[n, k] = size(M);
+
+SStotal = var(M(:)) *(n*k - 1);
+MSR = var(mean(M, 2)) * k;
+MSW = sum(var(M,0, 2)) / n;
+MSC = var(mean(M, 1)) * n;
+MSE = (SStotal - MSR *(n - 1) - MSC * (k -1))/ ((n - 1) * (k - 1));
+
+switch type
+    case '1-1'
+        [r, LB, UB, F, df1, df2, p] = ICC_case_1_1(MSR, MSE, MSC, MSW, alpha, r0, n, k);
+    case '1-k'
+        [r, LB, UB, F, df1, df2, p] = ICC_case_1_k(MSR, MSE, MSC, MSW, alpha, r0, n, k);
+    case 'C-1'
+        [r, LB, UB, F, df1, df2, p] = ICC_case_C_1(MSR, MSE, MSC, MSW, alpha, r0, n, k);
+    case 'C-k'
+        [r, LB, UB, F, df1, df2, p] = ICC_case_C_k(MSR, MSE, MSC, MSW, alpha, r0, n, k);
+    case 'A-1'
+        [r, LB, UB, F, df1, df2, p] = ICC_case_A_1(MSR, MSE, MSC, MSW, alpha, r0, n, k);
+    case 'A-k'
+        [r, LB, UB, F, df1, df2, p] = ICC_case_A_k(MSR, MSE, MSC, MSW, alpha, r0, n, k);
+end
+
+
+%----------------------------------------
+function [r, LB, UB, F, df1, df2, p] = ICC_case_1_1(MSR, MSE, MSC, MSW, alpha, r0, n, k)
+r = (MSR - MSW) / (MSR + (k-1)*MSW);
+
+F = (MSR/MSW) * (1-r0)/(1+(k-1)*r0);
+df1 = n-1;
+df2 = n*(k-1);
+p = 1-fcdf(F, df1, df2);
+
+FL = (MSR/MSW) / finv(1-alpha/2, n-1, n*(k-1));
+FU = (MSR/MSW) * finv(1-alpha/2, n*(k-1), n-1);
+
+LB = (FL - 1) / (FL + (k-1));
+UB = (FU - 1) / (FU + (k-1));
+
+%----------------------------------------
+function [r, LB, UB, F, df1, df2, p] = ICC_case_1_k(MSR, MSE, MSC, MSW, alpha, r0, n, k)
+r = (MSR - MSW) / MSR;
+
+F = (MSR/MSW) * (1-r0);
+df1 = n-1;
+df2 = n*(k-1);
+p = 1-fcdf(F, df1, df2);
+
+FL = (MSR/MSW) / finv(1-alpha/2, n-1, n*(k-1));
+FU = (MSR/MSW) * finv(1-alpha/2, n*(k-1), n-1);
+
+LB = 1 - 1 / FL;
+UB = 1 - 1 / FU;
+
+%----------------------------------------
+function [r, LB, UB, F, df1, df2, p] = ICC_case_C_1(MSR, MSE, MSC, MSW, alpha, r0, n, k)
+r = (MSR - MSE) / (MSR + (k-1)*MSE);
+
+F = (MSR/MSE) * (1-r0)/(1+(k-1)*r0);
+df1 = n - 1;
+df2 = (n-1)*(k-1);
+p = 1-fcdf(F, df1, df2);
+
+FL = (MSR/MSE) / finv(1-alpha/2, n-1, (n-1)*(k-1));
+FU = (MSR/MSE) * finv(1-alpha/2, (n-1)*(k-1), n-1);
+
+LB = (FL - 1) / (FL + (k-1));
+UB = (FU - 1) / (FU + (k-1));
+
+%----------------------------------------
+function [r, LB, UB, F, df1, df2, p] = ICC_case_C_k(MSR, MSE, MSC, MSW, alpha, r0, n, k)
+r = (MSR - MSE) / MSR;
+
+F = (MSR/MSE) * (1-r0);
+df1 = n - 1;
+df2 = (n-1)*(k-1);
+p = 1-fcdf(F, df1, df2);
+
+FL = (MSR/MSE) / finv(1-alpha/2, n-1, (n-1)*(k-1));
+FU = (MSR/MSE) * finv(1-alpha/2, (n-1)*(k-1), n-1);
+
+LB = 1 - 1 / FL;
+UB = 1 - 1 / FU;
+
+%----------------------------------------
+function [r, LB, UB, F, df1, df2, p] = ICC_case_A_1(MSR, MSE, MSC, MSW, alpha, r0, n, k)
+r = (MSR - MSE) / (MSR + (k-1)*MSE + k*(MSC-MSE)/n);
+
+a = (k*r0) / (n*(1-r0));
+b = 1 + (k*r0*(n-1))/(n*(1-r0));
+F = MSR / (a*MSC + b*MSE);
+%df2 = (a*MSC + b*MSE)^2/((a*MSC)^2/(k-1) + (b*MSE)^2/((n-1)*(k-1)));
+
+a = k*r/(n*(1-r));
+b = 1+k*r*(n-1)/(n*(1-r));
+v = (a*MSC + b*MSE)^2/((a*MSC)^2/(k-1) + (b*MSE)^2/((n-1)*(k-1)));
+
+df1 = n - 1;
+df2 = v;
+p = 1-fcdf(F, df1, df2);
+
+Fs = finv(1-alpha/2, n-1, v);
+LB = n*(MSR - Fs*MSE)/(Fs*(k*MSC + (k*n - k - n)*MSE) + n*MSR);
+
+Fs = finv(1-alpha/2, v, n-1);
+UB = n*(Fs*MSR-MSE)/(k*MSC + (k*n - k - n)*MSE + n*Fs*MSR);
+
+%----------------------------------------
+function [r, LB, UB, F, df1, df2, p] = ICC_case_A_k(MSR, MSE, MSC, MSW, alpha, r0, n, k)
+r = (MSR - MSE) / (MSR + (MSC-MSE)/n);
+
+c = r0/(n*(1-r0));
+d = 1 + (r0*(n-1))/(n*(1-r0));
+F = MSR / (c*MSC + d*MSE);
+%df2 = (c*MSC + d*MSE)^2/((c*MSC)^2/(k-1) + (d*MSE)^2/((n-1)*(k-1)));
+
+a = k*r/(n*(1-r));
+b = 1+k*r*(n-1)/(n*(1-r));
+v = (a*MSC + b*MSE)^2/((a*MSC)^2/(k-1) + (b*MSE)^2/((n-1)*(k-1)));
+
+df1 = n - 1;
+df2 = v;
+p = 1-fcdf(F, df1, df2);
+
+Fs = finv(1-alpha/2, n-1, v);
+LB = n*(MSR - Fs*MSE)/(Fs*(MSC-MSE) + n*MSR);
+
+Fs = finv(1-alpha/2, v, n-1);
+UB = n*(Fs*MSR - MSE)/(MSC - MSE + n*Fs*MSR);
+
